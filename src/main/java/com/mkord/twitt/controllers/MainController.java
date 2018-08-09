557738de2406eb00f6aeae2a406efe5cd7b33599
mkord <mkordyumov@gmail.com>
2018-08-09 23:16:05 +0200

    - Controller now can return Json

--- src/main/java/com/mkord/twitt/controllers/MainController.java
+++ src/main/java/com/mkord/twitt/controllers/MainController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 @RestController
 public class MainController {
 
@@ -14,25 +17,61 @@ public class MainController {
 
     @RequestMapping("post")
     String post(@RequestParam String user, @RequestParam String msg) {
-        System.out.println("user = [" + user + "], msg = [" + msg + "]");
         return "user = [" + user + "], msg = [" + msg + "]";
     }
 
     @RequestMapping("wall")
-    String wall(@RequestParam String user) {
-        System.out.println("user = [" + user + "]");
-        return "The wall for the user = [" + user + "]";
+    Wall wall(@RequestParam String user) {
+        Wall wall = new Wall(user);
+        wall.addMessage("very old msg");
+        wall.addMessage("old msg");
+        wall.addMessage("fresh msg");
+        wall.addMessage("the most fresh msg");
+        return wall;
     }
 
     @RequestMapping("timeline")
     String timeline(@RequestParam String user) {
-        System.out.println("user = [" + user + "]");
         return "The timeline for the user = [" + user + "]";
     }
 
     @RequestMapping("follow")
     String follow(@RequestParam String user, @RequestParam String followUser) {
-        System.out.println("user = [" + user + "], followUser = [" + followUser + "]");
         return "The user = [" + user + "] wants to follow user ["+followUser+"]";
+    }
+
+    private class Wall {
+        String owner;
+        Collection<String> messages;
+
+        public Wall() {
+            messages = new LinkedList<>();
+        }
+
+        Wall(String owner) {
+            this.owner = owner;
+            messages = new LinkedList<>();
+        }
+
+        public Wall(String owner, Collection<String> messages) {
+            this.owner = owner;
+            this.messages = messages;
+        }
+
+        public void setOwner(String owner) {
+            this.owner = owner;
+        }
+
+        void addMessage(String msg) {
+            messages.add(msg);
+        }
+
+        public String getOwner() {
+            return owner;
+        }
+
+        public Collection<String> getMessages() {
+            return messages;
+        }
     }
 }
