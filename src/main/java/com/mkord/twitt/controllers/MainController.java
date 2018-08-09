220717244d1bf0460cbddd142d966beb950c45bd
mkord <mkordyumov@gmail.com>
2018-08-09 22:56:26 +0200

    - added main rest end points

--- src/main/java/com/mkord/twitt/controllers/MainController.java
+++ src/main/java/com/mkord/twitt/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.mkord.twitt.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,29 @@ public class MainController {
     @RequestMapping("/")
     String usage() {
         return "Usage text will be here";
+    }
+
+    @RequestMapping("post")
+    String post(@RequestParam String user, @RequestParam String msg) {
+        System.out.println("user = [" + user + "], msg = [" + msg + "]");
+        return "user = [" + user + "], msg = [" + msg + "]";
+    }
+
+    @RequestMapping("wall")
+    String wall(@RequestParam String user) {
+        System.out.println("user = [" + user + "]");
+        return "The wall for the user = [" + user + "]";
+    }
+
+    @RequestMapping("timeline")
+    String timeline(@RequestParam String user) {
+        System.out.println("user = [" + user + "]");
+        return "The timeline for the user = [" + user + "]";
+    }
+
+    @RequestMapping("follow")
+    String follow(@RequestParam String user, @RequestParam String followUser) {
+        System.out.println("user = [" + user + "], followUser = [" + followUser + "]");
+        return "The user = [" + user + "] wants to follow user ["+followUser+"]";
     }
 }
